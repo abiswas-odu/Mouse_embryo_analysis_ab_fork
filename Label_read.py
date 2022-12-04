5615d70523a73929a595555537f2626f5baf5f05
abiswas-odu <ab.prof@gmail.com>
2022-12-04 05:50:44 -0500

    Automatic camera alignment with --align_camera

--- Label_read.py
+++ Label_read.py
@@ -18,37 +18,44 @@ def read_segments(data_dir, file_prefix, file_ext, segmentation_type):
     # nuclei
     try:
         if segmentation_type == "membrane":
-            file_prefix = file_prefix.split(os.extsep)[0]
-            mem_file = os.path.join(data_dir, file_prefix + ".crop_cp_masks" + file_ext)
-            if os.path.exists(mem_file):
-                label_mask = read_image(mem_file)
-            else: # Check the other cam
-                if 'Long' in file_prefix:
-                    replace_cam_prefix = file_prefix.replace('Long','Short')
-                else:
-                    replace_cam_prefix = file_prefix.replace('Short','Long')
-                mem_file = os.path.join(data_dir, replace_cam_prefix + ".crop_cp_masks" + file_ext)
-                if os.path.exists(mem_file):
-                    label_mask = read_image(mem_file)
+            label_mask = read_image(construct_membrane_file(data_dir, file_prefix, file_ext))
         else:
-            nucl_segm_file = os.path.join(data_dir,file_prefix + ".label" + file_ext)
-            nucl_segm_file_corrected = os.path.join(data_dir,file_prefix + "_SegmentationCorrected" + file_ext)
-            if os.path.exists(nucl_segm_file_corrected): # Look for corrected mask first
-                label_mask = read_image(nucl_segm_file_corrected)
-            elif os.path.exists(nucl_segm_file):
-                label_mask = read_image(nucl_segm_file)
-            else: # Check the other Cam
-                if 'Long' in file_prefix:
-                    replace_cam_prefix = file_prefix.replace('Long','Short')
-                else:
-                    replace_cam_prefix = file_prefix.replace('Short','Long')
-                nucl_segm_file = os.path.join(data_dir,replace_cam_prefix + ".label" + file_ext)
-                nucl_segm_file_corrected = os.path.join(data_dir,replace_cam_prefix + "_SegmentationCorrected" + file_ext)
-                if os.path.exists(nucl_segm_file_corrected): # Look for corrected mask first
-                    label_mask = read_image(nucl_segm_file_corrected)
-                elif os.path.exists(nucl_segm_file):
-                    label_mask = read_image(nucl_segm_file)
+            label_mask = read_image(construct_nucl_file(data_dir, file_prefix, file_ext))
     except Exception as e:
             print('Problem with reading segments', e)
-
     return label_mask
+
+
+def construct_membrane_file(data_dir, file_prefix, file_ext):
+    file_prefix = file_prefix.split(os.extsep)[0]
+    mem_file = os.path.join(data_dir, file_prefix + ".crop_cp_masks" + file_ext)
+    if os.path.exists(mem_file):
+        return mem_file
+    else: # Check the other cam
+        if 'Long' in file_prefix:
+            replace_cam_prefix = file_prefix.replace('Long','Short')
+        else:
+            replace_cam_prefix = file_prefix.replace('Short','Long')
+        mem_file = os.path.join(data_dir, replace_cam_prefix + ".crop_cp_masks" + file_ext)
+        if os.path.exists(mem_file):
+            return mem_file
+
+
+def construct_nucl_file(data_dir, file_prefix, file_ext):
+    nucl_segm_file = os.path.join(data_dir,file_prefix + ".label" + file_ext)
+    nucl_segm_file_corrected = os.path.join(data_dir,file_prefix + "_SegmentationCorrected" + file_ext)
+    if os.path.exists(nucl_segm_file_corrected): # Look for corrected mask first
+        return nucl_segm_file_corrected
+    elif os.path.exists(nucl_segm_file):
+        return nucl_segm_file
+    else: # Check the other Cam
+        if 'Long' in file_prefix:
+            replace_cam_prefix = file_prefix.replace('Long','Short')
+        else:
+            replace_cam_prefix = file_prefix.replace('Short','Long')
+        nucl_segm_file = os.path.join(data_dir,replace_cam_prefix + ".label" + file_ext)
+        nucl_segm_file_corrected = os.path.join(data_dir,replace_cam_prefix + "_SegmentationCorrected" + file_ext)
+        if os.path.exists(nucl_segm_file_corrected): # Look for corrected mask first
+            return nucl_segm_file_corrected
+        elif os.path.exists(nucl_segm_file):
+            return nucl_segm_file
--- tf_extract.py
+++ tf_extract.py
@@ -2,8 +2,9 @@ import click
 from TF_quant_simple import *
 from time import time
 from Align_cameras import *
+from Label_read import *
 
-__version__ = "1.2"
+__version__ = "1.3"
 
 @click.group()
 def cli():
@@ -13,7 +14,7 @@ def cli():
 @click.option('--orig_image_dir',required=True,
               type=click.Path(exists=True,file_okay=False,dir_okay=True,readable=True),
               help="Original klb/tif/h5/npy files.")
-@click.option('--nucl_seg_dir',required=False, default='',
+@click.option('--nucl_seg_dir',required=True, default='',
               type=click.Path(file_okay=False,dir_okay=True,readable=True),
               help="Directory with nuclei segmentation in same format as original images, klb/tif/h5/npy files.")
 @click.option('--membrane_seg_dir',required=False, default='',
@@ -35,12 +36,62 @@ def cli():
               help="The cell volume below which cells are not considered.")
 @click.option("--max_margin", required=False, default=2048, type=click.INT,
               help="Original size of images.")
-@click.option("--x_shift", required=False, default=-11, type=click.INT,
+@click.option("--align_camera", required=True, type=click.BOOL,
+              help="Align the camera.")
+@click.option("--x_shift_override", required=False, default=0, type=click.INT,
               help="X shift for camera alignment. For our 210809 data, for Cdx2, Masha found x_shift = -11.")
-@click.option("--y_shift", required=False, default=15, type=click.INT,
+@click.option("--y_shift_override", required=False, default=0, type=click.INT,
               help="Y shift for camera alignment. For our 210809 data, for Cdx2, Masha found y_shift = 15")
 def tf_align_simple(orig_image_dir, nucl_seg_dir, membrane_seg_dir,  crop_dir, cropbox_index,
-                timestamp_min, timestamp_max, offset, cell_volume_cutoff, max_margin, x_shift, y_shift):
+                timestamp_min, timestamp_max, offset, cell_volume_cutoff, max_margin, align_camera,
+                    x_shift_override, y_shift_override):
+
+    x_shift = 0
+    y_shift = 0
+    if align_camera:
+        images = [os.path.join(dp, f)
+                  for dp, dn, filenames in os.walk(orig_image_dir)
+                  for f in filenames if (os.path.splitext(f)[1] == '.klb' or
+                                         os.path.splitext(f)[1] == '.h5' or
+                                         os.path.splitext(f)[1] == '.tif' or
+                                         os.path.splitext(f)[1] == '.npy')]
+        images = np.sort(images)
+
+        click.echo('Attempting to align cameras...')
+        t0 = time()
+        image_1_idx = 0
+        cur_name = os.path.basename(str(images[image_1_idx]))
+        file_prefix = os.path.splitext(cur_name)[0]
+        file_ext = os.path.splitext(cur_name)[1]
+        nucl_seg_file = construct_nucl_file(nucl_seg_dir, file_prefix, file_ext)
+        x_shift_1, y_shift_1 = align_cameras(str(images[image_1_idx]), nucl_seg_file, crop_dir,
+                                         cropbox_index, offset, max_margin)
+
+        image_mid_idx = int(len(images)/2)
+        cur_name = os.path.basename(images[image_mid_idx])
+        file_prefix = os.path.splitext(cur_name)[0]
+        file_ext = os.path.splitext(cur_name)[1]
+        nucl_seg_file = construct_nucl_file(nucl_seg_dir, file_prefix, file_ext)
+        x_shift_1, y_shift_1 = align_cameras(images[image_mid_idx], nucl_seg_file, crop_dir,
+                                             cropbox_index, offset, max_margin)
+
+        image_1_idx = 0
+        cur_name = os.path.basename(images[image_1_idx])
+        file_prefix = os.path.splitext(cur_name)[0]
+        file_ext = os.path.splitext(cur_name)[1]
+        nucl_seg_file = construct_nucl_file(nucl_seg_dir, file_prefix, file_ext)
+        x_shift_1, y_shift_1 = align_cameras(images[image_1_idx], nucl_seg_file, crop_dir,
+                                             cropbox_index, offset, max_margin)
+
+        print('X-shift:' + str(x_shift))
+        print('Y-shift:' + str(y_shift))
+
+        t1 = time() - t0
+        click.echo("Time elapsed: " + str(t1))
+    else:
+        x_shift = x_shift_override
+        y_shift = y_shift_override
+
     click.echo('Extracting transcription factor intensity...')
     t0 = time()
     mem_tf_vals, nuc_tf_vals, mem_vols, nuc_vols = quantify_tf(orig_image_dir, membrane_seg_dir, nucl_seg_dir, crop_dir,
@@ -65,35 +116,6 @@ def tf_align_simple(orig_image_dir, nucl_seg_dir, membrane_seg_dir,  crop_dir, c
 
     t1 = time() - t0
     click.echo('Transcription factor intensity files generated here:' + crop_dir)
-    click.echo("Time elapsed: " + str(t1))
-
-
-@cli.command()
-@click.option('--orig_image_file',required=True,
-              type=click.Path(exists=True,file_okay=True,dir_okay=False,readable=True),
-              help="Original klb/tif/h5/npy file.")
-@click.option('--nucl_seg_file',required=False, default='',
-              type=click.Path(exists=True,file_okay=True,dir_okay=False,readable=True),
-              help="Directory with nuclei segmentation in same format as original images, klb/tif/h5/npy file.")
-@click.option('--crop_dir',required=True,
-              type=click.Path(exists=True,file_okay=False,dir_okay=True,readable=True),
-              help="The directory with hpair.csv and vpair.csv generated with generate-cropboxes. "
-                   "This is also the OUTPUT directory.")
-@click.option("--cropbox_index","-cbi", required=False, default=0, type=click.INT,
-              help="The cropbox to visualize for cropping.")
-@click.option("--offset","-of", required=False, default=150, type=click.INT,
-              help="The offset used during cropping.")
-@click.option("--max_margin", required=False, default=2048, type=click.INT,
-              help="Original size of images.")
-def align_cam(orig_image_file, nucl_seg_file, crop_dir, cropbox_index, offset, max_margin):
-    click.echo('Attempting to align cameras...')
-    t0 = time()
-    x_shift, y_shift = align_cameras(orig_image_file, nucl_seg_file, crop_dir,
-                                           cropbox_index, offset, max_margin)
-    print('X-shift:' + str(x_shift))
-    print('Y-shift:' + str(y_shift))
-
-    t1 = time() - t0
     click.echo("Time elapsed: " + str(t1))
 
 if __name__ == '__main__':
