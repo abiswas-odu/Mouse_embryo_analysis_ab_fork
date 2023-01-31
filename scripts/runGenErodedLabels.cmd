f2ba6661c416b35d71bbe1e13f8326afa38f1eef
Abhishek Biswas <abiswas-odu>
2023-01-31 13:32:00 -0500

    Fixed SLURM script.

--- scripts/runGenErodedLabels.cmd
+++ scripts/runGenErodedLabels.cmd
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name=gen_crops     # create a short name for your job
+#SBATCH --job-name=erode_labels  # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --mem=10G                # total memory per node
-#SBATCH --time=1:00:00           # total run time limit (HH:MM:SS)
+#SBATCH --mem=40G                # total memory per node
+#SBATCH --time=12:00:00          # total run time limit (HH:MM:SS)
 #SBATCH -A molbio
 
 
-IMAGE_PATH="/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork/test/data"
-NUCL_SEG_DIR="/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork/test/data"
+IMAGE_PATH="/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork/test/image_data"
+NUCL_SEG_DIR="/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork/test/nucl_seg"
 OUT_DIR="/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork/test/output"
 
 ##===================================================================================================
@@ -35,7 +35,7 @@ conda activate /projects/LIGHTSHEET/posfailab/ab50/tools/tf2-posfai
 
 SCRIPT_PATH=/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork
 
-${SCRIPT_PATH}/tf_extract.py erode-labels \
+python ${SCRIPT_PATH}/tf_extract.py erode-labels \
   --orig_image_dir ${IMAGE_PATH} \
   --nucl_seg_dir ${NUCL_SEG_DIR} \
   --out_dir ${OUT_DIR} \
