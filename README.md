0d7bc667dd1fd0b968514a789579c35d3e43f7d0
abiswas-odu <ab.prof@gmail.com>
2023-02-01 16:45:35 -0500

    Adding extract script and README update.

--- README.md
+++ README.md
@@ -1,20 +1,39 @@
 # Mouse_embryo_analysis
 
-## Extracting transcription factor signal
+## Extracting transcription factor signal on Della
 
+Executing the TF extraction program is best done using the scripts named ``runTFExtract_xxx.cmd`` in the scripts directory.
+
+## Eroding labels to better fit the nuclei on Della
+
+Executing the Erosion program is best done using the script ``runGenErodedLabels.cmd`` in the scripts directory.
+
+## View help on Della
+```commandline
+module purge
+module load anaconda3/2020.11
+export LD_LIBRARY_PATH=/projects/LIGHTSHEET/posfailab/ab50/tools/keller-lab-block-filetype/build/src
+conda activate /projects/LIGHTSHEET/posfailab/ab50/tools/tf2-posfai
+python /tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork tf_extract.py --help
+```
+
+## Installing on your own laptop
+
+Download the package
+```commandline
+git pull https://github.com/abiswas-odu/Mouse_embryo_analysis_ab_fork.git@princeton_release
+```
 Load the `segmentation` conda environment and run the help:
 
 ```commandline
+cd Mouse_embryo_analysis_ab_fork
 conda activate segmentation 
 python tf_extract.py tf-align-simple --help
 ```
 
-Now, if you're wondering what the segmentation environment is, then please follow the instructions here to install it: [Install segmentation python environment](https://github.com/abiswas-odu/roi_convertor#install-on-your-own-machine)
+Now, if you're wondering what the `segmentation` environment is, then please follow the instructions here to install it: [Install segmentation python environment](https://github.com/abiswas-odu/roi_convertor#install-on-your-own-machine)
 
-Executing the program requires using the commandline and parameters:
 
-```commandline
-python tf_extract.py tf-align-simple --orig_image_dir <dir> --nucl_seg_dir <label_dir> --crop_dir <crop_dir> --cropbox_index 7 --timestamp_min 0 --timestamp_max 10 --offset 150 --cell_volume_cutoff 2000
-```
 
-See --help for all the other parameters.
+
+
---
+++ scripts/runTFExtract_nuclei.cmd
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#SBATCH --job-name=erode_labels  # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --mem=40G                # total memory per node
+#SBATCH --time=12:00:00          # total run time limit (HH:MM:SS)
+#SBATCH -A molbio
+
+
+IMAGE_DIR="/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork/test/image_data"
+NUCL_SEG_DIR="/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork/test/nucl_seg"
+CROP_DIR="/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork/test/output"
+timestamp_min="0"
+timestamp_max="10"
+
+##===================================================================================================
+##=====================CHANGES BELOW THIS LINE FOR ADVANCED USERS====================================
+##===================================================================================================
+
+crop_index="0"
+offset="150"
+cell_volume_cutoff="2000"
+
+##===================================================================================================
+##==============================NO CHANGES BELOW THIS LINE===========================================
+##===================================================================================================
+
+echo Running on host `hostname`
+echo Starting Time is `date`
+echo Directory is `pwd`
+starttime=$(date +"%s")
+
+module purge
+module load anaconda3/2020.11
+export LD_LIBRARY_PATH=/projects/LIGHTSHEET/posfailab/ab50/tools/keller-lab-block-filetype/build/src
+conda activate /projects/LIGHTSHEET/posfailab/ab50/tools/tf2-posfai
+
+SCRIPT_PATH=/tigress/LIGHTSHEET/posfailab/ab50/tools/Mouse_embryo_analysis_ab_fork
+
+python ${SCRIPT_PATH}/tf_extract.py tf-align-simple \
+  --orig_image_dir ${IMAGE_DIR} \
+  --nucl_seg_dir ${NUCL_SEG_DIR} \
+  --crop_dir ${CROP_DIR} \
+  --cropbox_index ${crop_index} \
+  --timestamp_min ${timestamp_min} \
+  --timestamp_max ${timestamp_max} \
+  --offset ${offset} \
+  --cell_volume_cutoff ${cell_volume_cutoff} \
+  --max_margin ${image_size} \
+  --align_camera 1
+
+echo Ending time is $(date)
+endtime=$(date +"%s")
+diff=$(($endtime - $starttime))
+echo Elapsed time is $(($diff/60)) minutes and $(($diff%60)) seconds.
+
